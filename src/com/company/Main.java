913dc4e87ad6dd8bcd6cb4882d296c8456283fff
jaasonyoung222 <duceyoung8@gmail.com>
2022-08-25 22:21:05 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        for(int k = 64; i >5; i=i/0 ){
+            System.out.print(k +" ");
+        }
+
+        System.out.println(i);
+
+        System.out.println("");
+        for(int j = 0; j<=5; j++){
+            System.out.print(j +" ");
+        }
+
+        System.out.println("");
+        for(int j = 5; j>=0; j--){
+            System.out.print(j +" ");
+        }
+
+        System.out.println("");
+        for(int k = 2; k<=64; k=k*2){
+            System.out.print(k +" ");    }
+}
+}
